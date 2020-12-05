1e2d331b9fbf328c392120672c9f77737a77619c
BuildTools <unconfigured@null.spigotmc.org>
2020-12-05 00:30:57 -0500

    Add project files.

---
+++ Stoner_Alex_Exceptions/Exceptions.cpp
@@ -0,0 +1,28 @@
+// Alex Stoner
+// CIS 1202 502
+// December 4, 2020
+
+#include<iostream>
+#include<string>
+#include<cctype>
+using namespace std;
+
+char character(char start, int offset);
+
+int main() {
+	int offset;
+	char stop, start, end;
+
+
+
+	return 0;
+}
+
+char character(char start, int offset) {
+	try {
+
+	}
+	catch (){
+
+	}
+}
