709724155f50ca6d883cd432c0b20b6251632026
BuildTools <unconfigured@null.spigotmc.org>
2020-12-05 01:18:44 -0500

    Created driver in main using a do while loop and made a template for the character function.

--- Stoner_Alex_Exceptions/Exceptions.cpp
+++ Stoner_Alex_Exceptions/Exceptions.cpp
@@ -11,18 +11,35 @@ char character(char start, int offset);
 
 int main() {
 	int offset;
-	char stop, start, end;
+	char driver, start, end;
 
+	do {
+		cout << "Enter a letter: ";
+		cin >> start;
+		cout << "Enter the offset: ";
+		cin >> offset;
 
+		end = character(start, offset);
+		if (end != 0) {
+			cout << end << endl << endl;
+		}
+
+		cout << "Continue? Y/N";
+		cin >> driver;
+		cout << endl;
+	} while (driver == 'Y' || driver == 'y');
 
 	return 0;
 }
 
 char character(char start, int offset) {
+	char temp = start + offset;
+
 	try {
 
 	}
-	catch (){
-
+	catch (string s){
+		cout << s << endl;
+		return 0;
 	}
 }
